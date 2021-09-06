a90a15e0a7c6495e797510dda8837a6b51f35316
sneidermendoza <mariasol0304@gmail.com>
2021-09-06 17:58:18 -0500

    mejora del ejercicio

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,22 +1,22 @@
 # Encontrar el menor valor de un conjunto de n números dados
 
+set_number = int(input("Por favor ingrese cuantos numeros hay en su conjunto  de numeros: "))
+minor_number = int(input("ingrese el primer # 1:  "))
+
+for i in range(set_number -1):
+    number = int(input(f"ingrese el # {i + 2}: "))
+    if number < minor_number:
+        minor_number = number  
+
+
+print(f"Dentro del conjunto de numeros, el menor # es :{minor_number}") 
+
 # set_number = int(input("Por favor ingrese cuantos numeros hay en su conjunto  de numeros: "))
 # minor_number = 100000000000
 
 # for i in range(set_number):
-#     number = int(input(f"ingrese el # {i + 1}: "))
+#     number = float(input(f"ingrese el # {i + 1}: "))
 #     if number < minor_number:
 #         minor_number = number
-
-# print(f"Dentro del conjunto de numeros, el menor # es :{minor_number}") 
-
-set_number = int(input("Por favor ingrese cuantos numeros hay en su conjunto  de numeros: "))
-minor_number = 100000000000
-
-for i in range(set_number):
-    number = float(input(f"ingrese el # {i + 1}: "))
-    if number < minor_number:
-        minor_number = number
       
-
-print(f"Dentro del conjunto de numeros, el menor # es :{minor_number}")
+# print(f"Dentro del conjunto de numeros, el menor # es :{minor_number}")
